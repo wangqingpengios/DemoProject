2729fe8fd5eb16fbbd80a2752be8296a5417b951
home <home@qpdeMacBook-Air.local>
2017-08-03 13:59:48 +0800

    测试

--- MyDemo/MyDemo/Classes/Home/Controller/HomeViewController.m
+++ MyDemo/MyDemo/Classes/Home/Controller/HomeViewController.m
@@ -8,7 +8,11 @@
 
 #import "HomeViewController.h"
 
-@interface HomeViewController ()
+static NSString * cellID = @"cellID";
+
+@interface HomeViewController ()<UITableViewDelegate,UITableViewDataSource>
+
+@property (nonatomic, strong) UITableView *tableView;
 
 @end
 
@@ -17,22 +21,44 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    [self setupUI];
     
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - 设置界面
+- (void)setupUI {
+    
+    self.tableView.hidden = NO;
+    
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+#pragma mark - DataSource & Delegate
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 10;
 }
-*/
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:cellID forIndexPath:indexPath];
+    
+    
+        cell.textLabel.text =  [NSString stringWithFormat:@"%zd-%zd",indexPath.section,indexPath.row];
+        
+    
+    return cell;
+}
+
+- (UITableView *)tableView {
+    if (!_tableView) {
+        self.tableView = [[UITableView alloc]initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.height - 48) style:UITableViewStylePlain];
+        [self.view addSubview:self.tableView];
+        self.tableView.delegate = self;
+        self.tableView.dataSource = self;
+        self.tableView.tableFooterView = [UIView new];
+        [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:cellID];
+    }
+    return _tableView;
+}
+
+
 
 @end
